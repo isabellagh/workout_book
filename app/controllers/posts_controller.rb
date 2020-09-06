c2896080927b5d6ba39cb5edfad63c8e29f593df
isabellagh <isabellagh88@gmail.com>
2020-09-06 12:19:40 -0400

    dry post edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 
   get '/posts' do
     @posts = Post.all
-    erb :'posts/index'
+      erb :'posts/index'
   end
 
   get '/posts/new' do
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
       erb :"posts/new"
     else 
       flash[:error] = "You must be logged in to create a post!"
-      redirect "/"
+        redirect "/"
     end 
   end 
 
@@ -18,39 +18,35 @@ class PostsController < ApplicationController
     post = Post.new(title: params[:title], image_url: params[:image_url], description: params[:description], user_id: current_user.id)
     if post.save
       flash[:message] = "Post created successfully"
-      redirect "/posts/#{post.id}"
+        redirect "/posts/#{post.id}"
     else
       flash[:error] = "Post creation failed. Please fill out all inputs."
-      redirect "/posts/new" 
+        redirect "/posts/new" 
     end 
   end 
 
   get '/posts/:id' do
     find_post
-    erb :"/posts/show"
+      erb :"/posts/show"
   end 
   
   get '/posts/:id/edit' do
     find_post
-    if authorized_to_edit?(@post)
+    @post.user == current_user
       erb :'/posts/edit'
-    else 
-      flash[:error] = "Not authorized to edit this post!"
-      redirect "/posts"
-    end 
   end 
 
   patch '/posts/:id' do
     find_post
     @post.update(title: params[:title], image_url: params[:image_url],
     description: params[:description])
-    redirect "/posts/#{@post.id}"
+      redirect "/posts/#{@post.id}"
   end 
 
   delete '/posts/:id' do
     find_post
     @post.destroy
-    redirect '/posts'
+      redirect '/posts'
   end
 
   private
--- app/controllers/users_controler.rb
+++ app/controllers/users_controler.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   get '/login' do
     @users = User.all
-    erb :login
+     erb :login
   end
   
   post '/login' do
@@ -11,10 +11,10 @@ class UsersController < ApplicationController
       if @user && @user.authenticate(params[:password])
         session[:user_id] = @user.id #assign to
         flash[:message] = "Welcome back, #{@user.name}!"
-        redirect "/users/#{@user.id}"
+          redirect "/users/#{@user.id}"
       else 
         flash[:error] = "Invalid credentials, please try again!"
-        redirect '/login'
+          redirect '/login'
       end 
   end
 
@@ -22,16 +22,16 @@ class UsersController < ApplicationController
     if params[:name] != "" && params[:email] != "" && params[:password] != "" && params[:image_url] != "" && params[:bio] != ""
       @user = User.create(params)
       session[:user_id] = @user.id #logs the user in
-      redirect "/users/#{@user.id}"
+        redirect "/users/#{@user.id}"
     else
       flash[:error] = "Please fill out all fields"
-      redirect '/signup'
+        redirect '/signup'
     end 
   end 
 
   get "/users/:id" do
     @user = User.find_by(id: params[:id])
-    erb :show
+      erb :show
   end
 
   get '/signup' do
@@ -40,30 +40,30 @@ class UsersController < ApplicationController
 
   get '/profiles' do
     @users = User.all
-    erb :index
+      erb :index
   end
   
   get '/users/:id/editprofile' do
     @user = User.find(params[:id])
-    erb :edit
+      erb :edit
   end 
 
   patch '/users/:id' do
     @user = User.find(params[:id])
     @user.update(name: params[:name], image_url: params[:image_url],
     bio: params[:bio])
-    redirect "/users/#{@user.id}"
+      redirect "/users/#{@user.id}"
   end 
   
   get '/logout' do
     session.clear
-    redirect '/'
+      redirect '/'
   end 
 
   delete '/users/:id' do
     @user = User.find(params[:id])
     @user.destroy
-    redirect '/'
+      redirect '/'
   end 
 
 end 
